05d2a161819075093f9a2e5547ccda493d5beecb
Anton Stepin <ooosg@rambler.ru>
2022-04-29 15:30:21 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write first number: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Write second number:  ");
+double number2 = Convert.ToDouble(Console.ReadLine()); ;
+double max = number1;
+if (number2 > number1)
+{
+    max = number2;
+}
+Console.Write("Max number = ");
+Console.WriteLine(max);
