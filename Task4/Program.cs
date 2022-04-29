c483f0240ea73e764e6f816be7cf2c64a2e62748
Anton Stepin <ooosg@rambler.ru>
2022-04-29 23:56:16 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("Enter number for watch all even numbers since 1 and up to yours: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Catch it: ");
+for (int i = 2; i <= n; i = i + 2)
+{
+    Console.Write(i);
+    if (i != n & i != n - 1)
+        Console.Write(", ");
+}
