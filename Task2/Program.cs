04c5d6f56b8c4c338f6b802f629f2c137e379a53
Anton Stepin <ooosg@rambler.ru>
2022-04-29 21:26:21 +0300

    Task2ForThree

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,23 +1,22 @@
 ﻿Console.WriteLine("How many numbers you have?");
-int n = Convert.ToInt32(Console.ReadLine()) - 1;
-if (n > 10)
+int n = Convert.ToInt32(Console.ReadLine()) - 1; //n - quantity of numbers for comparison//
+while (n >= 3)
 {
-Console.WriteLine("So many numbers....come on....");
-Console.WriteLine("Try again (no more than ten please) ");
-n = Convert.ToInt32(Console.ReadLine()) - 1;
+    Console.WriteLine("So many numbers....come on....");
+    Console.WriteLine("Try again (no more than three please) ");
+    n = Convert.ToInt32(Console.ReadLine()) - 1;
 }
-else
+Console.WriteLine("Print Number ");
+double max = Convert.ToDouble(Console.ReadLine());
+for (int count = 0; count < n; count++)
+{
     Console.WriteLine("Print Number ");
-    double max = Convert.ToDouble(Console.ReadLine());
-    for (int count = 0; count < n; count++)
+    double temp = Convert.ToDouble(Console.ReadLine());
+    if (temp > max)
     {
-        Console.WriteLine("Print Number ");
-        double temp = Convert.ToDouble(Console.ReadLine());
-        if (temp > max)
-        {
-            max = temp;
-        }
+        max = temp;
     }
-    Console.Write("Max Number is ");
-    Console.Write(max);
-    Console.Write(" !!!");
+}
+Console.Write("Max Number is ");
+Console.Write(max);
+Console.Write(" !!!");
